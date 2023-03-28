775c869db9b9b169562a669b862b5d120990097d
sakib <s.sarder232002@gmail.com>
2023-03-29 01:11:28 +0600

    header done

---
+++ player-auction/src/Component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="navbar bg-neutral">
+        <a className="btn normal-case text-2xl">PlayerAuction</a>
+      </div>
+    </>
+  );
+};
+
+export default Header;
